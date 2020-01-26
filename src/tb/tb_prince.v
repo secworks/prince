20289e8a6b58fb9e5e221e21e67cc21a7cc63f12
Joachim Strömbergson <joachim@secworks.se>
2020-01-26 22:45:38 +0100

    Adding top level wrapper and associated testbench for the prince core.

---
+++ src/tb/tb_prince.v
@@ -0,0 +1,397 @@
+//======================================================================
+//
+// tb_prince.v
+// ---------
+// Testbench for the prince top level wrapper
+//
+//
+// Author: Joachim Strombergson
+// Copyright (c) 2019, Assured AB
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//======================================================================
+
+//------------------------------------------------------------------
+// Test module.
+//------------------------------------------------------------------
+module tb_prince();
+
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  parameter DEBUG     = 0;
+  parameter DUMP_WAIT = 0;
+
+  parameter CLK_HALF_PERIOD = 1;
+  parameter CLK_PERIOD = 2 * CLK_HALF_PERIOD;
+
+  localparam ADDR_NAME0        = 8'h00;
+  localparam ADDR_NAME1        = 8'h01;
+  localparam ADDR_VERSION      = 8'h02;
+
+  localparam ADDR_CTRL         = 8'h08;
+  localparam CTRL_NEXT_BIT     = 1;
+
+  localparam ADDR_STATUS       = 8'h09;
+  localparam STATUS_READY_BIT  = 0;
+
+  localparam ADDR_CONFIG       = 8'h0a;
+  localparam CONFIG_ENCDEC_BIT = 0;
+
+  localparam ADDR_KEY0         = 8'h10;
+  localparam ADDR_KEY1         = 8'h11;
+  localparam ADDR_KEY2         = 8'h12;
+  localparam ADDR_KEY3         = 8'h13;
+
+  localparam ADDR_BLOCK0       = 8'h20;
+  localparam ADDR_BLOCK1       = 8'h21;
+
+  localparam ADDR_RESULT0      = 8'h30;
+  localparam ADDR_RESULT1      = 8'h31;
+
+  //----------------------------------------------------------------
+  // Register and Wire declarations.
+  //----------------------------------------------------------------
+  reg [31 : 0] cycle_ctr;
+  reg [31 : 0] error_ctr;
+  reg [31 : 0] tc_ctr;
+  reg          tb_monitor;
+
+  reg           tb_clk;
+  reg           tb_reset_n;
+  reg           tb_cs;
+  reg           tb_we;
+  reg [7 : 0]   tb_address;
+  reg [31 : 0]  tb_write_data;
+  wire [31 : 0] tb_read_data;
+
+  reg [31 : 0] read_data;
+
+
+  //----------------------------------------------------------------
+  // Device Under Test.
+  //----------------------------------------------------------------
+  prince dut(
+           .clk(tb_clk),
+           .reset_n(tb_reset_n),
+
+           .cs(tb_cs),
+           .we(tb_we),
+
+           .address(tb_address),
+           .write_data(tb_write_data),
+           .read_data(tb_read_data)
+           );
+
+
+  //----------------------------------------------------------------
+  // clk_gen
+  //
+  // Always running clock generator process.
+  //----------------------------------------------------------------
+  always
+    begin : clk_gen
+      #CLK_HALF_PERIOD;
+      tb_clk = !tb_clk;
+    end // clk_gen
+
+
+  //----------------------------------------------------------------
+  // sys_monitor()
+  //
+  // An always running process that creates a cycle counter and
+  // conditionally displays information about the DUT.
+  //----------------------------------------------------------------
+  always
+    begin : sys_monitor
+      cycle_ctr = cycle_ctr + 1;
+      #(CLK_PERIOD);
+      if (tb_monitor)
+        begin
+          dump_dut_state();
+        end
+    end
+
+
+  //----------------------------------------------------------------
+  // dump_dut_state()
+  //
+  // Dump the state of the dump when needed.
+  //----------------------------------------------------------------
+  task dump_dut_state;
+    begin
+      $display("State of DUT");
+      $display("------------");
+      $display("Cycle: %08d", cycle_ctr);
+      $display("Inputs and outputs:");
+      $display("");
+    end
+  endtask // dump_dut_state
+
+
+  //----------------------------------------------------------------
+  // reset_dut()
+  //
+  // Toggle reset to put the DUT into a well known state.
+  //----------------------------------------------------------------
+  task reset_dut;
+    begin
+      $display("*** Toggle reset.");
+      tb_reset_n = 0;
+      #(2 * CLK_PERIOD);
+      tb_reset_n = 1;
+    end
+  endtask // reset_dut
+
+
+  //----------------------------------------------------------------
+  // display_test_result()
+  //
+  // Display the accumulated test results.
+  //----------------------------------------------------------------
+  task display_test_result;
+    begin
+      if (error_ctr == 0)
+        begin
+          $display("*** All %02d test cases completed successfully", tc_ctr);
+        end
+      else
+        begin
+          $display("*** %02d tests completed - %02d test cases did not complete successfully.",
+                   tc_ctr, error_ctr);
+        end
+    end
+  endtask // display_test_result
+
+
+  //----------------------------------------------------------------
+  // init_sim()
+  //
+  // Initialize all counters and testbed functionality as well
+  // as setting the DUT inputs to defined values.
+  //----------------------------------------------------------------
+  task init_sim;
+    begin
+      cycle_ctr  = 0;
+      error_ctr  = 0;
+      tc_ctr     = 0;
+      tb_monitor = 0;
+
+      tb_clk        = 1'h0;
+      tb_reset_n    = 1'h1;
+      tb_cs         = 1'h0;
+      tb_we         = 1'h0;
+      tb_address    = 8'h0;
+      tb_write_data = 32'h0;
+    end
+  endtask // init_sim
+
+
+  //----------------------------------------------------------------
+  // write_word()
+  //
+  // Write the given word to the DUT using the DUT interface.
+  //----------------------------------------------------------------
+  task write_word(input [11 : 0] address,
+                  input [31 : 0] word);
+    begin
+      if (DEBUG)
+        begin
+          $display("*** Writing 0x%08x to 0x%02x.", word, address);
+          $display("");
+        end
+
+      tb_address = address;
+      tb_write_data = word;
+      tb_cs = 1;
+      tb_we = 1;
+      #(2 * CLK_PERIOD);
+      tb_cs = 0;
+      tb_we = 0;
+    end
+  endtask // write_word
+
+
+  //----------------------------------------------------------------
+  // read_word()
+  //
+  // Read a data word from the given address in the DUT.
+  // the word read will be available in the global variable
+  // read_data.
+  //----------------------------------------------------------------
+  task read_word(input [11 : 0]  address);
+    begin
+      tb_address = address;
+      tb_cs = 1;
+      tb_we = 0;
+      #(CLK_PERIOD);
+      read_data = tb_read_data;
+      tb_cs = 0;
+
+      if (DEBUG)
+        begin
+          $display("*** Reading 0x%08x from 0x%02x.", read_data, address);
+          $display("");
+        end
+    end
+  endtask // read_word
+
+
+  //----------------------------------------------------------------
+  // wait_ready()
+  //
+  // Wait for the ready flag to be set in dut.
+  //----------------------------------------------------------------
+  task wait_ready;
+    begin : wready
+      read_word(ADDR_STATUS);
+      while (read_data == 0)
+        read_word(ADDR_STATUS);
+    end
+  endtask // wait_ready
+
+
+  //----------------------------------------------------------------
+  // tc1()
+  //----------------------------------------------------------------
+  task tc1;
+    begin : tc1
+      reg [31 : 0] res0;
+      reg [31 : 0] res1;
+
+      tc_ctr = tc_ctr + 1;
+
+      $display("*** TC1 - encryption started.");
+
+      write_word(ADDR_KEY0, 32'h00010203);
+      write_word(ADDR_KEY1, 32'h04050607);
+      write_word(ADDR_KEY2, 32'h08090a0b);
+      write_word(ADDR_KEY3, 32'h0c0d0e0f);
+
+      write_word(ADDR_BLOCK0, 32'h41424344);
+      write_word(ADDR_BLOCK1, 32'h45464748);
+
+      write_word(ADDR_CONFIG, 32'h1);
+      write_word(ADDR_CTRL, 32'h2);
+
+      wait_ready();
+
+      read_word(ADDR_RESULT0);
+      res0 = read_data;
+      read_word(ADDR_RESULT1);
+      res1 = read_data;
+
+      if ({res0, res1} == 64'h497df3d072612cb5)
+        begin
+          $display("*** Correct result received.");
+        end
+      else
+        begin
+          $display("*** Incorrect result received. Expexted 64'h497df3d072612cb5, got 0x%016x", {res0, res1});
+          error_ctr = error_ctr + 1;
+        end
+
+      $display("*** TC1 completed.");
+      $display("");
+    end
+  endtask // tc1
+
+
+  //----------------------------------------------------------------
+  // tc2()
+  //----------------------------------------------------------------
+  task tc2;
+    begin : tc2
+      reg [31 : 0] res0;
+      reg [31 : 0] res1;
+
+      tc_ctr = tc_ctr + 1;
+
+      $display("*** TC2 - decryption started.");
+
+      write_word(ADDR_KEY0, 32'h00010203);
+      write_word(ADDR_KEY1, 32'h04050607);
+      write_word(ADDR_KEY2, 32'h08090a0b);
+      write_word(ADDR_KEY3, 32'h0c0d0e0f);
+
+      write_word(ADDR_BLOCK0, 32'h497df3d0);
+      write_word(ADDR_BLOCK1, 32'h72612cb5);
+
+      write_word(ADDR_CONFIG, 32'h0);
+      write_word(ADDR_CTRL, 32'h2);
+
+      wait_ready();
+
+      read_word(ADDR_RESULT0);
+      res0 = read_data;
+      read_word(ADDR_RESULT1);
+      res1 = read_data;
+
+      if ({res0, res1} == 64'h4142434445464748)
+        begin
+          $display("*** Correct result received.");
+        end
+      else
+        begin
+          $display("*** Incorrect result received. Expexted 64'h4142434445464748, got 0x%016x", {res0, res1});
+          error_ctr = error_ctr + 1;
+        end
+
+      $display("*** TC2 completed.");
+      $display("");
+    end
+  endtask // tc2
+
+
+  //----------------------------------------------------------------
+  // prince_core_test
+  //
+  // Test vectors from:
+  //----------------------------------------------------------------
+  initial
+    begin : prince_test
+      $display("   -= Testbench for prince started =-");
+      $display("     ============================");
+      $display("");
+
+      init_sim();
+      reset_dut();
+
+      tc1();
+      tc2();
+
+      display_test_result();
+      $display("");
+      $display("*** prince simulation done. ***");
+      $finish;
+    end // prince_test
+endmodule // tb_prince
+
+//======================================================================
+// EOF tb_prince.v
+//======================================================================
